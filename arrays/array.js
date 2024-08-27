7ff45627c6ac9157491e4417977a55687f4dd310
Saurabh10 Tiwari <Saurabh10.Tiwari@ril.com>
2024-08-27 16:36:27 +0530

    Arrays more ques

--- arrays/array.js
+++ arrays/array.js
@@ -32,4 +32,71 @@ function returnElement (arr, n) {
     let newArr = arr.slice(0,n)
     return newArr
 }
-console.log(returnElement([8,2,3,5],0))
+console.log(returnElement("First elements",[8,2,3,5],0))
+
+//return last element of array if n is passed then return last n elements 
+
+function returnLastElement (arr,n) {
+    if(n===0 || n == null){
+        return arr[arr.length-1]
+    }
+    const lastArr = arr.slice(Math.max(arr.length -n ,0))
+    return lastArr
+}
+console.log(returnLastElement("last elements",[8,2,3,5],3))
+
+//Join Array elements to a String
+function arrtoString (array) {
+   return array.join('+')
+}
+console.log(arrtoString(["red", "green", "yellow"]))
+
+//Function to sort the array
+
+function soretedArray(arr){
+    return arr.sort((a,b)=> a-b)
+}
+console.log(soretedArray( [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ]))
+
+//Function for most frequent element in an array
+
+function freqElem (arr) {
+    let frequency = 1;
+    let m;
+    let item;
+    for(let i =0; i<arr.length; i++){
+        for(let j =0 ; j<arr.length; j++){
+            if(arr[i] == arr[j]){
+                m++
+            }
+            if(frequency<m){
+                frequency = m;
+                item = arr[i]
+            }
+        }
+        m = 0;
+
+    }  
+    return {frequency, item}
+}
+
+console.log(freqElem([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
+
+//Function to swap the case of each alphabet in a String 
+
+function swapElem (str) {
+    let str1 = str.split('')
+    // console.log(str1)
+    str1.map(element => {
+        // console.log(element.toUpperCase())
+        if(element === element.toUpperCase()){
+            element.toLowerCase();
+        }else{
+            element.toUpperCase()
+        }
+    });
+    str1.join('')
+    // console.log("str1",str1)
+    return str
+}
+console.log(swapElem('The Quick Brown Fox'))
